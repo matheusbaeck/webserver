fe94903a7514a20d232fd81c9bfabceb6f8bd99a
matheusbaeck <matheusbaeck@gmail.com>
2024-10-12 01:04:47 +0200

    v02

--- Selector.cpp
+++ Selector.cpp
@@ -37,32 +37,64 @@ void Selector::addSocket(const Worker &worker)
 	LogMessage(DEBUG);
 }
 
-void	Selector::processEvents(std::queue<Request>& requestQueue)
+void Selector::processEvents(std::queue<Request>& requestQueue)
 {
-	m_nfds = epoll_wait(m_epollfd, m_events, MAX_EVENTS, TIME_OUT);
-	if (m_nfds == -1) {
-		oss() << "epoll_wait failed";
-		LogMessage(ERROR);
-		return;
-	}
+    m_nfds = epoll_wait(m_epollfd, m_events, MAX_EVENTS, TIME_OUT);
+    if (m_nfds == -1) {
+        oss() << "epoll_wait failed: " << strerror(errno);
+        LogMessage(ERROR);
+        return;
+    }
 
-	for (int n = 0; n < m_nfds; ++n) {
-		int event_fd = m_events[n].data.fd;
+    for (int n = 0; n < m_nfds; ++n) {
+        int event_fd = m_events[n].data.fd;
 
-		Worker* worker = getWorkerByFd(event_fd);
-		if (!worker) {
-			LogMessage(ERROR, "processEvents: No worker found for fd");
-			continue;
-		}
+        Worker* worker = getWorkerByFd(event_fd);
+        if (!worker) {
+            LogMessage(ERROR, "processEvents: No worker found for fd " + std::to_string(event_fd));
+            continue;
+        }
 
-		if (m_events[n].events & EPOLLIN) {
-			Request request(event_fd, worker->port());
-			requestQueue.push(request);
+        if (m_events[n].events & EPOLLIN) {
+            if (event_fd == worker->sock()) {
+                // This is a listening socket, accept the new connection
+                int client_fd = worker->accept_connection();
+                if (client_fd < 0) {
+                    oss() << "Failed to accept new connection: " << strerror(errno);
+                    LogMessage(ERROR);
+                    continue;
+                }
 
-			oss() << "processEvents: Added request for fd " << event_fd << " to queue";
-			LogMessage(INFO);
-		}
-	}
+                // Add the new client socket to epoll
+                epoll_event ev;
+                ev.events = EPOLLIN | EPOLLET;  // Edge-triggered mode
+                ev.data.fd = client_fd;
+                if (epoll_ctl(m_epollfd, EPOLL_CTL_ADD, client_fd, &ev) == -1) {
+                    oss() << "Failed to add client socket to epoll: " << strerror(errno);
+                    LogMessage(ERROR);
+                    close(client_fd);
+                    continue;
+                }
+
+                oss() << "Accepted new connection on fd " << client_fd;
+                LogMessage(INFO);
+            } else {
+                // This is a client socket, create a request
+                Request request(event_fd, worker->port());
+                requestQueue.push(request);
+
+                oss() << "processEvents: Added request for fd " << event_fd << " to queue";
+                LogMessage(INFO);
+            }
+        }
+        
+        if (m_events[n].events & (EPOLLERR | EPOLLHUP)) {
+            oss() << "Error or hangup on fd " << event_fd << ": " << strerror(errno);
+            LogMessage(ERROR);
+            epoll_ctl(m_epollfd, EPOLL_CTL_DEL, event_fd, NULL);
+            close(event_fd);
+        }
+    }
 }
 
 Worker*	Selector::getWorkerByFd(int fd) const
